69264a33f9285be73e70432df39614a9a6658e68
marty <techno.mooney@gmail.com>
2020-09-23 03:08:07 -0500

    made changes to the looks of the code

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import os
 import discord
 from discord.ext import commands as c
 from dotenv import load_dotenv
-import json
 import requests
 
 
-command_prefix='pb!'
+command_prefix = 'pb!'
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv("DISCORD_GUILD")
@@ -26,23 +25,28 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})\n'
         )
 
+
 @client.event
 async def on_member_join(member):
     print(f'{member} has joined {GUILD}')
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'pong! \n responded in {round(client.latency * 1000,4)} ms')
 
+
 @client.command(aliases=['8ball'])
-async def _8ball(ctx,*,question):
+async def _8ball(ctx, *, question):
     answer = requested_8ball(question)
     await ctx.send(answer['answer'])
     await ctx.send(f'the response type was {answer["type"]}')
+
 
 def requested_8ball(question):
     url = 'https://8ball.delegator.com/magic/JSON/'
     response = requests.get(url+question).json()
     return response['magic']
+
 
 client.run(TOKEN)
